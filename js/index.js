7c50b3d341263f9a44a4f409de575e70eae41b3a
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-02 15:36:34 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+// messing around with adding event listeners and displaying variables after
+input.addEventListener('keydown', function(e) {
+    var info = '';
+    info = info + e.key;
+    if (e.key === 'enter') {
+      console.log(info);
+    }
+});
