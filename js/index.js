016a8d90cefdd79c19856c74996d311124c06181
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-02 15:31:49 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+
+input.addEventListener('keydown', function(e) {
+    var info = '';
+    info = info + e.key;
+    if (e.key === 'enter') {
+      console.log(info);
+    }
+});
